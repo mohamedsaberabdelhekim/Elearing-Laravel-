383e7a6bf7a8da7d3b06fa436226b6be9d04a1a0
Mohamed_Saber <33199103+mohamedsaberabdelhekim@users.noreply.github.com>
2022-04-22 03:25:22 +0200

    iti project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div
+        style="background-image: url('images/about_us_single_template_378254.jpg');width:100%;height:90vh;background-size:cover;">
+
+    </div>
+@endsection
---
+++ resources/views/start.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div style="background-image:
+                    url('/images/guia-elearning-1024x484-2.jpg');
+                    background-size:cover;background-position:center; width:100%; height:85vh; margin-top:10px">
+
+    </div>
+@endsection
